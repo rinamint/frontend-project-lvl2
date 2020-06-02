788dd9b5717126fc33f5f0585826a8dde9a8de0c
rina <mintnight1@gmail.com>
2020-06-02 21:42:43 +0300

    update

---
+++ src/diff.js
@@ -0,0 +1,32 @@
+import _ from 'lodash';
+import fs from 'fs';
+import path from 'path';
+
+export default (path1, path2) => {
+  const pathToObject1 = path.isAbsolute(path1) ? path1 : path.resolve(process.cwd(), path1);
+  const pathToObject2 = path.isAbsolute(path2) ? path2 : path.resolve(process.cwd(), path2);
+  const file1 = JSON.parse(fs.readFileSync(pathToObject1));
+  const file2 = JSON.parse(fs.readFileSync(pathToObject2));
+  const keys1 = Object.keys(file1);
+  const keys2 = Object.keys(file2);
+  const keys = _.union(keys1, keys2);
+
+  const diff = keys.reduce((acc, key) => {
+    const before = file1[key];
+    const after = file2[key];
+    if (before === after) {
+      acc.push([`${key}: ${before}`]);
+    }
+    if (before !== after) {
+      if (after === undefined) {
+        acc.push([`-${key}: ${before}`]);
+      } else {
+        acc.push([`-${key}: ${before}`]);
+        acc.push([`+${key}: ${after}`]);
+      }
+    }
+    return acc;
+  }, []);
+
+  return diff.join('\n');
+};
--- src/gendiff.js
+++ src/gendiff.js
@@ -1,13 +1,16 @@
 #!/usr/bin/env node
 import program from 'commander';
-
+import diffrenece from './diff.js';
 
 program
-    .description('Compares two configuration files and shows a difference')
-    .version('1.0.0', '-V, --version', 'output the version number')
-    .helpOption('-h, --help', 'output usage information')
-    .option('-f, --format <type>', 'output format')
-    
-
+  .description('Compares two configuration files and shows a difference')
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<firstPath> <secondPath>')
+  .action((firstPath, secondPath) => {
+    const result = diffrenece(firstPath, secondPath);
+    console.log(result);
+  });
 
 program.parse(process.argv);
