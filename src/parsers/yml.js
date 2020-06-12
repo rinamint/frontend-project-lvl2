6983b5766c990592d7c2e95b40fb14c1256f8048
rina <mintnight1@gmail.com>
2020-06-12 16:25:17 +0300

    update

--- src/parsers/yml.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-import yaml from 'js-yaml';
-
-
-export default (filepath1, filepath2) => {
-  const file1 = yaml.safeLoad(fs.readFileSync(filepath1));
-  const file2 = yaml.safeLoad(fs.readFileSync(filepath2));
-  return [file1, file2];
-};
