0ae107fbf7f9e6737d5390679fc184cbda5974fe
rina <mintnight1@gmail.com>
2020-06-17 16:04:26 +0300

    update

--- src/diff.js
+++ src/diff.js
@@ -3,28 +3,45 @@ import getFiles from './parsers/parsing.js';
 
 // eslint-disable-next-line consistent-return
 
-export default (path1, path2) => {
-  const [firstFile, secondFile] = getFiles(path1, path2);
-  const keys1 = Object.keys(firstFile);
-  const keys2 = Object.keys(secondFile);
-  const keys = _.union(keys1, keys2);
-
-  const diff = keys.reduce((acc, key) => {
-    const before = firstFile[key];
-    const after = secondFile[key];
-    if (before === after) {
-      acc.push([`    ${key}: ${before}`]);
-    }
-    if (before !== after) {
-      if (after === undefined) {
-        acc.push([`    -${key}: ${before}`]);
-      } else {
-        acc.push([`    -${key}: ${before}`]);
-        acc.push([`    +${key}: ${after}`]);
+const difference = (object1, object2) => {
+  const keysOfFirst = Object.keys(object1);
+  const keysOfSecond = Object.keys(object2);
+  const keys = _.union(keysOfFirst, keysOfSecond);
+  console.log(keys)
+  const transformation = {};
+  const diff = keys.map((key) => {
+    const first = object1[key];
+    const second = object2[key];
+    if (_.has(object1, key) && _.has(object2, key)) {
+      if (typeof (first) === 'object' && typeof (second) === 'object') {
+        transformation.name = key;
+        transformation.value = difference(first, second);
+        return transformation;
+      }
+      if (first === second) {
+        transformation.name = key;
+        transformation.value = first.repeat(2);
+        return transformation;
       }
     }
-    return acc;
-  }, []);
+    if (first === undefined) {
+      transformation.name = key;
+      transformation.value = second;
+      transformation.modification = '+';
+      return transformation;
+    }
+    if (second === undefined) {
+      transformation.name = key;
+      transformation.value = first;
+      transformation.modification = '-';
+      return transformation;
+    }
+  });
+  return diff;
+};
 
-  return `{\n${diff.join('\n')}\n}`;
+export default (path1, path2) => {
+  const [firstObj, secondObj] = getFiles(path1, path2);
+  const transition = difference(firstObj, secondObj);
+  return transition;
 };
