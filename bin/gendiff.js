dad2b0ba27c774b86f2a0b570138af6b5322e94c
rina <mintnight1@gmail.com>
2020-07-10 13:22:44 +0300

    update

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,12 +4,11 @@ import diffrenece from '../src/diff.js';
 
 program
   .description('Compares two configuration files and shows a difference')
-  .version('1.0.0', 'output the version number')
+  .version('1.0.0')
   .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<firstPath> <secondPath>')
   .action((firstPath, secondPath) => {
     const result = diffrenece(firstPath, secondPath, program.format);
     console.log(result);
-  });
-
-program.parse(process.argv);
+  })
+  .parse(process.argv);
