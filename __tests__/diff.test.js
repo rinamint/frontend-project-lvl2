10a1b7570d0ebb1662f7c567b15579b9fa1e12d7
rina <mintnight1@gmail.com>
2020-08-14 19:05:12 +0300

    update

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -39,22 +39,6 @@ test.each(files)('compare two files', (file1, file2) => {
   expect(generateDiff(before, after, 'json')).toEqual(expectedJSON);
 });
 
-test('INI', () => {
-  const before = getPath('before.ini');
-  const after = getPath('after.ini');
-  expect(generateDiff(before, after, 'stylish')).toEqual(expectedStylish);
-  expect(generateDiff(before, after, 'plain')).toEqual(expectedPlain);
-  expect(generateDiff(before, after, 'json')).toEqual(expectedJSON);
-});
-
-test('YML', () => {
-  const before = getPath('before.yml');
-  const after = getPath('after.yml');
-  expect(generateDiff(before, after, 'stylish')).toEqual(expectedStylish);
-  expect(generateDiff(before, after, 'plain')).toEqual(expectedPlain);
-  expect(generateDiff(before, after, 'json')).toEqual(expectedJSON);
-});
-
 test('JSON nested', () => {
   const beforeTree = getPath('treeBefore.json');
   const afterTree = getPath('treeAfter.json');
--- src/index.js
+++ src/index.js
@@ -4,19 +4,7 @@ import parse from './parsing.js';
 import formatter from './formatters/formatter.js';
 import generateDiff from './difference.js';
 
-const readFiles = (pathToFile) => {
-  switch (path.extname(pathToFile)) {
-    case '.json':
-      return [fs.readFileSync(pathToFile), 'json'];
-    case '.yml':
-      return [fs.readFileSync(pathToFile, 'utf-8'), 'yml'];
-    case '.ini':
-      return [fs.readFileSync(pathToFile, 'utf-8'), 'ini'];
-    default:
-      throw new Error(`'Wrong filepath: ${pathToFile}'`);
-  }
-};
-
+const readFiles = (pathToFile) => [fs.readFileSync(pathToFile, 'utf-8'), path.extname(pathToFile).slice(1)];
 
 export default (path1, path2, format) => {
   const [firstFile, firstFormat] = readFiles(path1);
